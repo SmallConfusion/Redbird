97a05ecb080513ccd8f76dc84bd2a82d12f9551d
SmallConfusion <smallconfusion@protonmail.com>
2024-06-14 13:44:26 -0500

    Added music a bit

--- main.gd
+++ main.gd
@@ -3,6 +3,7 @@ extends Control
 
 func _ready():
 	load_main()
+	MusicManager.updated.connect(on_music_manager_updated)
 
 
 func _process(_delta):
@@ -11,3 +12,7 @@ func _process(_delta):
 func load_main():
 	var instance = preload("res://game/test.tscn").instantiate()
 	%SubViewport.add_child(instance)
+
+
+func on_music_manager_updated():
+	MusicManager.play("music", "main")
