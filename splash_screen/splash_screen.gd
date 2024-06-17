d571e1823b7cc062b9692576163ca6847de99487
SmallConfusion <smallconfusion@protonmail.com>
2024-06-17 17:48:22 -0500

    Made splash screen better

--- splash_screen/splash_screen.gd
+++ splash_screen/splash_screen.gd
@@ -2,21 +2,20 @@ extends CanvasLayer
 
 
 func _ready():
-	if not OS.is_debug_build():
+	if not OS.is_debug_build() or true:
 		get_tree().paused = true
-		await get_tree().create_timer(2).timeout
+		await get_tree().create_timer(3).timeout
 		get_tree().paused = false
 		await get_tree().create_timer(0.05).timeout
 		get_tree().paused = true
 		
 		var tween = get_tree().create_tween()
 		tween.set_pause_mode(Tween.TWEEN_PAUSE_PROCESS)
-		tween = tween.tween_property($SplashScreen, "modulate", Color(1, 1, 1, 0), 2)
 		tween.set_trans(Tween.TRANS_CUBIC)
-		tween.set_ease(Tween.EASE_IN_OUT)
+		tween.set_ease(Tween.EASE_IN)
+		tween.tween_property($SplashScreen, "position", Vector2(0, 720), 1)
 		
 		await tween.finished
-		
 		get_tree().paused = false
 	
 	queue_free()
