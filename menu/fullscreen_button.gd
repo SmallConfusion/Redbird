dfc321321a379abbd0f409dd1e13587528b4b071
SmallConfusion <smallconfusion@protonmail.com>
2024-06-19 08:16:35 -0500

    Added fullscreen button

--- menu/fullscreen_button.gd
+++ menu/fullscreen_button.gd
@@ -5,11 +5,8 @@ var scene_manager
 
 func _ready():
 	connect("focus_exited", _focus_exited)
-
-
-func init(scene_manager_):
-	scene_manager = scene_manager_
 	_set_text()
+
 
 func _focus_exited():
 	SoundManager.play("menu", "input")
@@ -17,9 +14,14 @@ func _focus_exited():
 
 func _pressed():
 	SoundManager.play("menu", "select")
-	scene_manager.set_performance_mode(not scene_manager.performance_mode)
+	
+	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_WINDOWED:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	
 	_set_text()
 
 
 func _set_text():
-	text = "Quality %s" % ("L" if scene_manager.performance_mode else "H")
+	text = "Fullscreen"
